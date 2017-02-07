0f77e76372f5720ff66243d6b5bb39828c7fbdf5
1597538426 <78059800@qq.com>
2017-02-07 12:46:30 +0800

    Initial Commit

---
+++ faceWindowDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  faceWindowDemo
+//
+//  Created by 周智伟 on 17/2/7.
+//  Copyright © 2017年 周智伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
